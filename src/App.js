9cf6cf92ebfb9937657f9d53c7c47ca2bd269121
Sergio-prog <sergioreklamplay@gmail.com>
2023-09-04 14:19:51 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./App.css";
+import { ethers } from "ethers";
+import Web3Provider from "./network";
+import NarBar from "./NavBar/NavBar";
+import CoinSwapper from "./CoinSwapper/CoinSwapper";
+import { Route } from "react-router-dom";
+import { SnackbarProvider } from "notistack";
+import Liquidity from "./Liquidity/Liquidity";
+import { createTheme, ThemeProvider } from "@material-ui/core";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#16a65e",
+      contrastText: "#ffffff",
+    },
+    secondary: {
+      main: "#7e8f86",
+      contrastText: "#ffffff",
+    },
+  },
+});
+
+const App = () => {
+  return (
+    <div className="App">
+      <SnackbarProvider maxSnack={3}>
+        <ThemeProvider theme={theme}>
+          <Web3Provider
+            render={(network) => (
+              <div>
+                <NarBar />
+                <Route exact path="/Alternative-Uniswap-Interface/">
+                  <CoinSwapper network={network} />
+                </Route>
+
+                <Route exact path="/Alternative-Uniswap-Interface/liquidity">
+                  <Liquidity network={network} />
+                </Route>
+              </div>
+            )}
+          ></Web3Provider>
+        </ThemeProvider>
+      </SnackbarProvider>
+    </div>
+  );
+};
+
+export default App;
